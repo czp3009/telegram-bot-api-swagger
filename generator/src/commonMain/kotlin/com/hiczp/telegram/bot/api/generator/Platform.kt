575e933682240a0173b772d90fa3e6848b333422
czp3009 <czp3009@gmail.com>
2026-02-14 04:01:14 +0800

    change to LF

--- generator/src/commonMain/kotlin/com/hiczp/telegram/bot/api/generator/Platform.kt
+++
@@ -1,9 +0,0 @@
-package com.hiczp.telegram.bot.api.generator
-
-internal expect fun getEnvironmentVariable(name: String): String?
-
-internal expect fun fileExists(path: String): Boolean
-
-internal expect fun createDirectory(path: String)
-
-internal expect fun writeFile(path: String, content: String)
--- generator/src/mingwX64Main/kotlin/com/hiczp/telegram/bot/api/generator/Platform.mingw.kt
+++
@@ -1,9 +0,0 @@
-package com.hiczp.telegram.bot.api.generator
-
-import kotlinx.cinterop.ExperimentalForeignApi
-import platform.posix.mkdir
-
-@OptIn(ExperimentalForeignApi::class)
-internal actual fun createDirectory(path: String) {
-    mkdir(path)
-}
